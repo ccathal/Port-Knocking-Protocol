3c550ba0abc22a5835c654bd661bce0531e56cda
Cathal Corbett <cathalcorbett3@gmail.com>
2020-01-25 12:15:47 +0000

    Removed unnecessary files from project

--- src/MyServer.java
+++ src/MyServer.java
@@ -23,29 +23,30 @@ import javax.crypto.spec.SecretKeySpec;
 public class MyServer extends Thread {
         
 	private DatagramSocket socket;
-	private static DatagramSocket connectionSocket;
-    private static boolean running;
-    private static byte[] buf = new byte[256];
-    private static ArrayList<Integer> knockingSequence = new ArrayList<Integer>();
-    private static ArrayList<Integer> connectionKnocks = new ArrayList<Integer>();
-    private final static ArrayList<Integer> confirmKnockingSequence =  new ArrayList<Integer>(Arrays.asList(5, 7000, 4000, 6543));
-    private static HashMap<AttemptKnockingSequence, ArrayList<SingleKnock>> hashKnock = new HashMap<AttemptKnockingSequence, ArrayList<SingleKnock>>();
-    private static ArrayList<SingleKnock> knockList = new ArrayList<>();
-    private static Logger logger = Logger.getLogger("MyLog");
-    private static FileHandler fh;
-    private static final String privKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDN+myZccwm9sll/Z4B56xBfNsAGX1ykwVP6+St2gSxForwZqmuM6xkLfZozgdmKdb9LaOZc14oGGd88aw2BPd09FMxosf1UCHYawZWaz3w8u9VM8GZkWWijmdNpw+pBaCbyqQ+hl9Q7K4Sqh2x+R/QJArkjzifQayisSY01sOUt4Dro5yA+2j/YsEypkA9PAY9qccMSlwQHXUiRyx0ceYQEu+7KNK9Cc5GkklzfdJT8vRfJH8xmd3CyBQAfGPxK8niR+Ail0FwFmKIcRlWMU52t71iFDh1NtAMFY4NdSx9c2ltYEQKKFBsLxp3B21VADagsOisemdrXRXcPOJxCschAgMBAAECggEATLM3xVvOvaOgE06BjAwM5MXtdvgG8qc0jzI0EVjh7l+KlUJlZOzxAMzsuNIfhzeFSvm3tehz41JTFv+XNPZcfzyLzivjccHJGKGh1oRQqGyOGpgPO3Qc+I82gH/5IONrjxfCWVYIIEZB+8lFDYTLB+Kj+8ApQYRfGKYGqB5g7ftL/YNmaUI1ntnIpssXrq08iHbEVA+jDrCvQj9glaJiF4Fxylvzd6Qep2Kh/7sYvEHmPPEaEOJhXOc6uxwSiMGU2eexjNxrQa3P5dv2rEDHgZArNoye5ZVVQpGqF5GagmY7sW5tHm9VkwRCVZo6s3uk7S/OWZmZAZPuuMkHpnU8kQKBgQDr/bDGp503zUfSfUuSlg09ARw0CsUV8vzBAy4tibW6mB1BZc34OZ6bvzww/+yCr6t9oIJKCSp2u0O+g6waYQtxWkrAO4crC+D12mVHLCHuTY2Y6VGw3UfZa3dkf/rYHiuon2KxRvMSirdmfEEJpO9qZx9AvXIntQWckPTzgE/6kwKBgQDfcUpzBo1+RAdM3IGB9Cua+nkMHaJnUmuKVxZH1ViZzfD3xBymMW69bCyqiFOVIoKaC6NEXmQ3qeWvjUZN9cuG8dlWkjLJlP9SNNMARsRE52qLHsG3nW2HI8wPW0EoZdF8qiJYgza10hdDlxz+CregtAsi0h9B/J2LXxbAU+oj+wKBgFvDzlWxH8VvIZqL9jMN/h/Wqqzh8zlRv08eeXpjrjLcq6OefrUjUrWlazZyjflTbg/vtjorzkNVFkai1O19BwIQ5jhR7YGjoNp5DiDa3GbZ6VGoiIeJxEKbM1X1Hgmj0b5EHBBrUmHHZwGHF5M0e5SYfOKjyBwAnCoBg/6byn3ZAoGAEZpQPi2W+gqL9K8ueLlusf/nh1/SSoeAt15TAAe7uioyQKKvixw72CpsfmbNBuO4HECsdRdml8gHs0PS9RNXHGNzNtG/tIfLcYN91/i7P55nk1wx8LAzT8EvM0qCIJec4FBa8lQr/Dj34jhGbXEUtFFayzx4f+9RzggIt9Akkv8CgYEA5EVrvJx5aUBpCD5ldmVOrAXtSNcgiaiSJJu48N6kgPK2iVWkYsLgkpdez1pVG1yl8t9p6UEjgXnFXSXwJB8kvRmaFZ/ef+xk08OSRBWVhYWivl5LoMwcJVje7RQeSiciVyuDgRT3NYDFjJCG4kLX0fbbsulHdWh71CdP2m2cyuc=";
-    		private static String operatingSystem = System.getProperty("os.name");
+	private DatagramSocket connectionSocket;
+    private boolean running;
+    private byte[] buf = new byte[256];
+    private ArrayList<Integer> knockingSequence = new ArrayList<Integer>();
+    private ArrayList<Integer> connectionKnocks = new ArrayList<Integer>();
+    private final ArrayList<Integer> confirmKnockingSequence =  new ArrayList<Integer>(Arrays.asList(5, 7000, 4000, 6543));
+    private HashMap<AttemptKnockingSequence, ArrayList<SingleKnock>> hashKnock = new HashMap<AttemptKnockingSequence, ArrayList<SingleKnock>>();
+    private ArrayList<SingleKnock> knockList = new ArrayList<>();
+    private Logger logger = Logger.getLogger("MyLog");
+    private FileHandler fh;
+    private final String privKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDN+myZccwm9sll/Z4B56xBfNsAGX1ykwVP6+St2gSxForwZqmuM6xkLfZozgdmKdb9LaOZc14oGGd88aw2BPd09FMxosf1UCHYawZWaz3w8u9VM8GZkWWijmdNpw+pBaCbyqQ+hl9Q7K4Sqh2x+R/QJArkjzifQayisSY01sOUt4Dro5yA+2j/YsEypkA9PAY9qccMSlwQHXUiRyx0ceYQEu+7KNK9Cc5GkklzfdJT8vRfJH8xmd3CyBQAfGPxK8niR+Ail0FwFmKIcRlWMU52t71iFDh1NtAMFY4NdSx9c2ltYEQKKFBsLxp3B21VADagsOisemdrXRXcPOJxCschAgMBAAECggEATLM3xVvOvaOgE06BjAwM5MXtdvgG8qc0jzI0EVjh7l+KlUJlZOzxAMzsuNIfhzeFSvm3tehz41JTFv+XNPZcfzyLzivjccHJGKGh1oRQqGyOGpgPO3Qc+I82gH/5IONrjxfCWVYIIEZB+8lFDYTLB+Kj+8ApQYRfGKYGqB5g7ftL/YNmaUI1ntnIpssXrq08iHbEVA+jDrCvQj9glaJiF4Fxylvzd6Qep2Kh/7sYvEHmPPEaEOJhXOc6uxwSiMGU2eexjNxrQa3P5dv2rEDHgZArNoye5ZVVQpGqF5GagmY7sW5tHm9VkwRCVZo6s3uk7S/OWZmZAZPuuMkHpnU8kQKBgQDr/bDGp503zUfSfUuSlg09ARw0CsUV8vzBAy4tibW6mB1BZc34OZ6bvzww/+yCr6t9oIJKCSp2u0O+g6waYQtxWkrAO4crC+D12mVHLCHuTY2Y6VGw3UfZa3dkf/rYHiuon2KxRvMSirdmfEEJpO9qZx9AvXIntQWckPTzgE/6kwKBgQDfcUpzBo1+RAdM3IGB9Cua+nkMHaJnUmuKVxZH1ViZzfD3xBymMW69bCyqiFOVIoKaC6NEXmQ3qeWvjUZN9cuG8dlWkjLJlP9SNNMARsRE52qLHsG3nW2HI8wPW0EoZdF8qiJYgza10hdDlxz+CregtAsi0h9B/J2LXxbAU+oj+wKBgFvDzlWxH8VvIZqL9jMN/h/Wqqzh8zlRv08eeXpjrjLcq6OefrUjUrWlazZyjflTbg/vtjorzkNVFkai1O19BwIQ5jhR7YGjoNp5DiDa3GbZ6VGoiIeJxEKbM1X1Hgmj0b5EHBBrUmHHZwGHF5M0e5SYfOKjyBwAnCoBg/6byn3ZAoGAEZpQPi2W+gqL9K8ueLlusf/nh1/SSoeAt15TAAe7uioyQKKvixw72CpsfmbNBuO4HECsdRdml8gHs0PS9RNXHGNzNtG/tIfLcYN91/i7P55nk1wx8LAzT8EvM0qCIJec4FBa8lQr/Dj34jhGbXEUtFFayzx4f+9RzggIt9Akkv8CgYEA5EVrvJx5aUBpCD5ldmVOrAXtSNcgiaiSJJu48N6kgPK2iVWkYsLgkpdez1pVG1yl8t9p6UEjgXnFXSXwJB8kvRmaFZ/ef+xk08OSRBWVhYWivl5LoMwcJVje7RQeSiciVyuDgRT3NYDFjJCG4kLX0fbbsulHdWh71CdP2m2cyuc=";
+    private String operatingSystem = System.getProperty("os.name");
     
     // open datagram socket on server with specisied port number
     public static void main(String[] args) throws IOException, InterruptedException {
     	String tcpDumpCmd = "/usr/sbin/tcpdump -A -l -n udp port '(5 or 7000 or 4000 or 6543)'";
-		runTCPDUmp(tcpDumpCmd, true);
+    	MyServer server = new MyServer();
+    	server.runTCPDUmp(tcpDumpCmd, true);
     }
  
-    public static void runTCPDUmp(String crunchifyCmd, boolean waitForResult) throws IOException, InterruptedException {
+    private void runTCPDUmp(String crunchifyCmd, boolean waitForResult) throws IOException, InterruptedException {
     	
-    	Process setup = Runtime.getRuntime().exec("iptables-save > IPTablesbackup.txt");
-		setup.waitFor();
+    	//Process setup = Runtime.getRuntime().exec("iptables-save > IPTablesbackup.txt");
+		//setup.waitFor();
 		
     	// set up logging file
         try {
@@ -130,7 +131,6 @@ public class MyServer extends Thread {
 
 					// when hashmap array of single knocks is 4 or less
 		            if (arr.size() < 5) {
-		            	
 		            	// add incoming knock to the attempt
 		            	SingleKnock single = new SingleKnock(Integer.parseInt(destPort), Long.parseLong(values[0]), Integer.parseInt(values[1]));
 		            	hashKnock.computeIfAbsent(aks, k -> new ArrayList<>()).add(single);
@@ -150,8 +150,7 @@ public class MyServer extends Thread {
 		            		if (knockingSequence.equals(confirmKnockingSequence)) {
 		            			logger.info("Correct Knock Sequence: IP - " + aks.getAddress() + ": Port - " + aks.getPort());
 		            			logger.info("Submitting connection ports for allowed connection: Connection Knocks - " + connectionKnocks);
-		            			//acceptClientConnection(connectionKnocks);
-		            			makeConnection(aks.getAddress(), aks.getPort(), 22);
+		            			makeConnection(aks.getAddress(), aks.getPort(), connectionKnocks);
 		            		} else {
 		            			// else, connection refused
 		            			logger.warning("Incorrect Knock Sequence: IP - " + aks.getAddress() + ": Port - " + aks.getPort());
@@ -180,34 +179,37 @@ public class MyServer extends Thread {
     }
 
 
-	private static void makeConnection(InetAddress srcAddress, int srcPort, int connectPort) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
+	private void makeConnection(InetAddress srcAddress, int srcPort, ArrayList<Integer> connectKnocks) throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
 		
 		Process connUDP, connTCP, connICMP;
 		Process dropUDP, dropTCP, dropICMP;
 		String address = srcAddress.getHostAddress().toString();
 		
-    	String acceptICMPConnectionCmd = "sudo iptables -I INPUT -p icmp -s " + address + " -j ACCEPT";
-    	String acceptTCPConnectionCmd = "sudo iptables -I INPUT -p tcp -s " + address + " --dport " + connectPort + " -j ACCEPT";
-    	String acceptUDPConnectionCmd = "sudo iptables -I INPUT -p udp -s " + address + " --dport " + connectPort + " -j ACCEPT";
-
-    	String dropICMPConnectionCmd = "sudo iptables -D INPUT -p icmp -s " + address+ " -j ACCEPT";
-    	String dropTCPConnectionCmd = "sudo iptables -D INPUT -p tcp -s " + address + " --dport " + connectPort + " -j ACCEPT";
-    	String dropUDPConnectionCmd = "sudo iptables -D INPUT -p udp -s " + address + " --dport " + connectPort + " -j ACCEPT";
-
-    	try {
-			connUDP = Runtime.getRuntime().exec(acceptUDPConnectionCmd);
-			connTCP = Runtime.getRuntime().exec(acceptTCPConnectionCmd);
-			connICMP = Runtime.getRuntime().exec(acceptICMPConnectionCmd);
-			
-			Thread.sleep(100000);
-			
-			dropUDP = Runtime.getRuntime().exec(dropUDPConnectionCmd);
-			dropTCP = Runtime.getRuntime().exec(dropTCPConnectionCmd);
-			dropICMP = Runtime.getRuntime().exec(dropICMPConnectionCmd);
-			//drop.waitFor();
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		for(int port : connectKnocks) {
+			String connectPort = Integer.toString(port);
+	    	String acceptICMPConnectionCmd = "sudo iptables -I INPUT -p icmp -s " + address + " -j ACCEPT";
+	    	String acceptTCPConnectionCmd = "sudo iptables -I INPUT -p tcp -s " + address + " --dport " + connectPort + " -j ACCEPT";
+	    	String acceptUDPConnectionCmd = "sudo iptables -I INPUT -p udp -s " + address + " --dport " + connectPort + " -j ACCEPT";
+	
+	    	String dropICMPConnectionCmd = "sudo iptables -D INPUT -p icmp -s " + address+ " -j ACCEPT";
+	    	String dropTCPConnectionCmd = "sudo iptables -D INPUT -p tcp -s " + address + " --dport " + connectPort + " -j ACCEPT";
+	    	String dropUDPConnectionCmd = "sudo iptables -D INPUT -p udp -s " + address + " --dport " + connectPort + " -j ACCEPT";
+	
+	    	try {
+				connUDP = Runtime.getRuntime().exec(acceptUDPConnectionCmd);
+				connTCP = Runtime.getRuntime().exec(acceptTCPConnectionCmd);
+				connICMP = Runtime.getRuntime().exec(acceptICMPConnectionCmd);
+				logger.info("Server switched connection port - " + connectPort); 
+				
+				Thread.sleep(100000);
+				
+				dropUDP = Runtime.getRuntime().exec(dropUDPConnectionCmd);
+				dropTCP = Runtime.getRuntime().exec(dropTCPConnectionCmd);
+				dropICMP = Runtime.getRuntime().exec(dropICMPConnectionCmd);
+				//drop.waitFor();
+			} catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
